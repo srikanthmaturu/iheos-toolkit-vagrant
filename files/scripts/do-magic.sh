abb471a0a981aee4a2e7731625f09d3dc3e99eb5
Srikanth Maturu <srikanthmaturu@users.noreply.github.com>
2022-02-10 15:47:10 -0600

    Fix comment

--- files/scripts/do-magic.sh
+++ files/scripts/do-magic.sh
@@ -1,7 +1,7 @@
 echo "Make directories to make sure they are there..."
 mkdir -p /etc/tomcat/Catalina/localhost
 
-echo "Downloading reference-cdda-validator war file..."
+echo "Downloading xdstools7.7.2 war file..."
 wget -nv https://github.com/usnistgov/iheos-toolkit2/releases/download/v7.7.2/xdstools7.7.2.war -O /var/lib/tomcat/webapps/xdstools7.7.2.war
 
 echo "Copying config_extras..."
